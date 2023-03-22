3e0a79834ba4a5764b1f1f0461fb650c4728ae2a
Romi <romi.peli@pm.me>
2023-03-22 19:36:19 +0100

    Initial Commit

---
+++ BeatBip/BeatBipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatBipApp.swift
+//  BeatBip
+//
+//  Created by Romi on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeatBipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
